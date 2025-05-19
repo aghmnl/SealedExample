bcede005940d90e656b1db84bf968358614fe60a
aghmnl <agustin.minetto@gmail.com>
2025-05-19 11:41:55 -0300

    sealed class GameError

--- app/src/main/java/com/agus/sealedexample/GameModel.kt
+++ app/src/main/java/com/agus/sealedexample/GameModel.kt
@@ -1,5 +1,11 @@
 package com.agus.sealedexample
 
-data class GameModel(val title: String, val serialNumber: Number, val error: String) {
+data class GameModel(val title: String, val serialNumber: Number, val error: GameError) {
 
+}
+
+sealed class GameError() {
+    object RayadoError : GameError()  // Los objetos de la sealed class suelen ir en mayúscula
+    object InternetError : GameError()
+    object NoError : GameError()
 }
--- app/src/main/java/com/agus/sealedexample/MainActivity.kt
+++ app/src/main/java/com/agus/sealedexample/MainActivity.kt
@@ -2,33 +2,31 @@ package com.agus.sealedexample
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import com.agus.sealedexample.GameError.*
 
 class MainActivity : ComponentActivity() {
 
     val gameList = listOf<GameModel>(
-        GameModel("Mario 1", 123452345, "error con internet"),
-        GameModel("Mario 2", 215648412, "rayado"),
-        GameModel("Mario 3", 785135744, "")
+        GameModel("Mario 1", 123452345, InternetError),
+        GameModel("Mario 2", 215648412, RayadoError),
+        GameModel("Mario 3", 785135744, NoError)
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         gameList.forEach { gameModel ->
             when (gameModel.error) {
-                "error con internet" -> llevarAlSoporteTecnico()
-                "rayado" -> eliminarJuego()
-                "" -> vender()
-                else -> niIdea()
-
+                InternetError -> llevarAlSoporteTecnico()
+                RayadoError -> eliminarJuego()
+                NoError -> vender()
             }
         }
     }
 
-    private fun niIdea() {}
+//    private fun niIdea() {}
     private fun vender() {}
     private fun eliminarJuego() {}
     private fun llevarAlSoporteTecnico() {}
-
 }
 
 
